60c36228b495486dc806e1438b3a4156c0ab5fc3
Guilherme Prestes <gprestes@thoughtworks.com>
2019-07-23 17:46:08 +0200

    Adds test to check that division by zero returns an expection

--- tests/test_math.py
+++ tests/test_math.py
@@ -12,3 +12,7 @@ def test_subtration():
         [(0, 5, 0), (1, 5, 5), (2, 5, 10), (-3, 5, -15), (-4, -5, 20)])
 def test_multiplication(a, b, expected):
     assert a * b == expected
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        1 / 0
