d31787aaaae35f8b69c37ceb9b6050410f90aa01
Guilherme Prestes <gprestes@thoughtworks.com>
2019-07-26 17:23:16 +0200

    browser is now in a config file

--- tests/test_web.py
+++ tests/test_web.py
@@ -6,11 +6,24 @@ from pages.search import DuckDuckGoSearchPage
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.keys import Keys
 
+import json
+
+
+@pytest.fixture(scope="session")
+def config():
+    with open("tests/config.json") as config_file:
+        data = json.load(config_file)
+    return data
+
 
 @pytest.fixture
-def browser():
-    driver = Chrome()
-    driver.implicitly_wait(10)
+def browser(config):
+    if config["browser"] == "chrome":
+        driver = Chrome()
+    else:
+        raise Exception(f'"{config["browser"]}" is not a supported browser')
+
+    driver.implicitly_wait(config["wait_time"])
     yield driver
     driver.quit()
 
